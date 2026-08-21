e7cc536d64dcf81e29a559ec785edaf74d2b5773
SuperWarden <plonisan6@gmail.com>
2026-08-21 15:16:59 +0300

    First commit

---
+++ src/main/java/org/superwarden/gdmstelegrambot/GdmsTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package org.superwarden.gdmstelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdmsTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdmsTelegramBotApplication.class, args);
+    }
+
+}
